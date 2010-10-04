75b3decb7ce912d5e4ee4696d11d2a1adce6abc7
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-10-04 02:51:47 +0000

    New package for query database

---
+++ trunk/Source code/finalProject/src/uit/tkorg/dbsa/model/Author.java
@@ -0,0 +1,59 @@
+/**
+ *
+ * @authort tiendv
+ * 
+ * 
+ */
+package uit.tkorg.dbsa.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Author implements Serializable{
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAuthorNum() {
+        return authorNum;
+    }
+
+    public void setAuthorNum(int authorNum) {
+        this.authorNum = authorNum;
+    }
+
+    public boolean isEditor() {
+        return editor;
+    }
+
+    public void setEditor(boolean editor) {
+        this.editor = editor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Publication getPublications() {
+        return publications;
+    }
+
+    public void setPublications(Publication publications) {
+        this.publications = publications;
+    }
+    
+    private int id;
+    private String author;
+    private boolean editor;
+    private int authorNum;
+    private Publication publications;
+}
