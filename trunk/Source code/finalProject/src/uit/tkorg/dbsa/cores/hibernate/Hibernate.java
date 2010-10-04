fb607f5847f772f3dc524751e7176cdde456da58
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-10-04 02:53:42 +0000

    add package hibernate

---
+++ trunk/Source code/finalProject/src/uit/tkorg/dbsa/cores/hibernate/Hibernate.java
@@ -0,0 +1,16 @@
+/**
+ *   @author tiendv
+ */
+package uit.tkorg.dbsa.cores.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+ 
+private static SessionFactory sessionFactory;
+
+static {
+	sessionFactory = new  Configuration().configure().buildSessionFactory();
+}
+public static SessionFactory getSessionFactory() {
+	return sessionFactory;
+}
