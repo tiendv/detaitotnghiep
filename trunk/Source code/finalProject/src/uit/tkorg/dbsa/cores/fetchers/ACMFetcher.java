a45519ad198c48113f6556e710a479c9d6da008e
cuongnp.uit.se@gmail.com <cuongnp.uit.se@gmail.com@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-10-13 16:39:25 +0000

    update fetch from ACM.

--- trunk/Source code/finalProject/src/uit/tkorg/dbsa/cores/fetchers/ACMFetcher.java
+++ trunk/Source code/finalProject/src/uit/tkorg/dbsa/cores/fetchers/ACMFetcher.java
@@ -1,18 +1,26 @@
 package uit.tkorg.dbsa.cores.fetchers;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.sf.jabref.BibtexEntry;
 import net.sf.jabref.Globals;
+import net.sf.jabref.imports.BibtexParser;
+import net.sf.jabref.imports.HTMLConverter;
+import net.sf.jabref.imports.ImportInspector;
+import net.sf.jabref.imports.ParserResult;
 
 import antlr.ByteBuffer;
 
@@ -21,6 +29,10 @@ public class ACMFetcher {
 	//Tu khoa can tim kiem
 	private String keywordString = null;
 	
+	private static String terms = null;
+	
+	private final static HTMLConverter htmlConverter = new HTMLConverter();
+	
 	private static String startUrl = "http://portal.acm.org/";
 	private static String searchUrlPart = "results.cfm?query=";
 	private static String searchUrlPartII = "&dl=";
@@ -28,24 +40,30 @@ public class ACMFetcher {
 	/**
 	 * ex: http://portal.acm.org/results.cfm?coll=ACM&dl=ACM&CFID=107834110&CFTOKEN=22371969
 	 */
-	
+	private static int MAX_FETCH = 20;
+	private static int perPage = MAX_FETCH;
 	private static int hits = 0;
 	
-	static Pattern hitsPattern = Pattern.compile(".*Found <b>(\\d+,*\\d*)</b> of.*");
-    Pattern maxHitsPattern = Pattern.compile(".*Results \\d+ - \\d+ of (\\d+,*\\d*).*");
+	private static Pattern hitsPattern = Pattern.compile(".*Found <b>(\\d+,*\\d*)</b> of.*");
+    private static Pattern maxHitsPattern = Pattern.compile(".*Results \\d+ - \\d+ of (\\d+,*\\d*).*");
+    private static Pattern bibPattern = Pattern.compile(".*(popBibTex.cfm.*)','BibTex'.*");
+	private static Pattern fullCitationPattern = Pattern.compile("<A HREF=\"(citation.cfm.*)\" class.*");
+	private static Pattern absPattern = Pattern.compile(".*ABSTRACT</A></span>\\s+<p class=\"abstract\">\\s+(.*)");
+	private static boolean shouldContinue = false;
+	private int unparseable = 0;
+	private static int parsed = 0;
 	
 	public ACMFetcher(){
 		
 	}
 	
-	public static URL MakeUrl(String keyword){
+	public static URL MakeUrl(int startIndex){
 		StringBuffer sb = new StringBuffer(startUrl); 
 		sb.append(searchUrlPart);		
-		sb.append(keyword.replaceAll(" ", "20%"));
+		sb.append(terms.replaceAll(" ", "20%"));
 		sb.append(searchUrlPartII);
 		sb.append("ACM");
 		sb.append(endUrl);
-		System.out.println(" sb.toString = " + sb.toString());
 		
 		URL urlSearch = null;
 		
@@ -55,44 +73,68 @@ public class ACMFetcher {
 			
 			e.printStackTrace();
 		}
-		System.out.println("urlsearch = " + urlSearch);
 		
 		return urlSearch;
 	}
 	
 	public static void Fetcher(String keyword) throws IOException{
 		
+		terms = keyword;
+	
 		/*
 		 * 
 		 */
-		URL url = MakeUrl(keyword);
+		URL url = MakeUrl(0);
+		
 		
 		/*
 		 * 
 		 */
+		System.out.println("Url = " + url);
+		
 		String page = getFetcherResult(url);
+		
+		System.out.println("page 1");
 		
 		GetResultList(page);
 		
-		FileOutputStream fileOutput;
-		DataOutputStream dataOutput;
-		File file = new File("C:\\abc.txt");
-		try {
-			fileOutput = new FileOutputStream(file);
-			dataOutput =  new DataOutputStream(fileOutput);
-			try {
-				dataOutput.writeChars(page);
-			} catch (IOException e) {
-				
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
+		parse(page, 0, 1);
 		
-			e.printStackTrace();
-		}
-		
-		
-		//System.out.println("---------====><====-----------" + page);
+		int firstEntry = perPage;
+        while (shouldContinue && (firstEntry < hits)) {
+            
+        	page = getFetcherResult(MakeUrl(firstEntry));
+        	
+        	//System.out.println("Fetching from: "+firstEntry);
+            
+//        	address = makeUrl(firstEntry);
+            //System.out.println(address);
+//            page = getResults(new URL(address));
+            
+            //dialog.setProgress(firstEntry+perPage/2, hits);
+            if (!shouldContinue)
+                break;
+
+            parse(page, 0, 1+firstEntry);
+            firstEntry += perPage;
+        }
+        
+//		FileOutputStream fileOutput;
+//		DataOutputStream dataOutput;
+//		File file = new File("C:\\abc.txt");
+//		try {
+//			fileOutput = new FileOutputStream(file);
+//			dataOutput =  new DataOutputStream(fileOutput);
+//			try {
+//				dataOutput.writeChars(page);
+//			} catch (IOException e) {
+//				
+//				e.printStackTrace();
+//			}
+//		} catch (FileNotFoundException e) {
+//		
+//			e.printStackTrace();
+//		}
 	}
 	
 	public static String getFetcherResult(URL url) throws IOException{
@@ -110,8 +152,6 @@ public class ACMFetcher {
 				sb.append((char)buffer[i]);
 			}
 		}
-
-		
 		return sb.toString();
 	}
 	
@@ -125,20 +165,27 @@ public class ACMFetcher {
 			}
 			
 			int index = page.indexOf("Found");
-			System.out.println("index = " + index);
+			
 			if (index >= 0) {
-				//System.out.println("page = " + page);
             	page = page.substring(index + 5);
-            	//System.out.println("page = index + 9 = " + page);
+            	
 				index = page.indexOf("Found");
 				if (index >= 0)
             		page = page.substring(index);
 			}
 			
-			//int maxHits = getNumberOfHits(page, "Results", maxHitsPattern);
+			try {
+				int maxHits = getNumberOfHits(page, "Results", maxHitsPattern);	
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+			
+			if(hits > MAX_FETCH){
+				hits = MAX_FETCH;
+			}
 		
 		
-		System.out.println(">''< ==> " + hits);
 	}
 	
 	private static int getNumberOfHits(String page, String marker, Pattern pattern) throws IOException {
@@ -168,5 +215,140 @@ public class ACMFetcher {
 	            }
 	        }
 	        throw new IOException(Globals.lang("Could not parse number of hits"));
-	    }
+	}
+	
+	static int piv = 0;
+	
+	private static void parse(String text, int startIndex, int firstEntryNumber) {
+        piv = startIndex;
+        int entryNumber = firstEntryNumber;
+        
+        BibtexEntry entry;
+        while (((entry = parseNextEntry(text, piv, entryNumber)) != null) && (shouldContinue)) {
+        
+        	if (entry.getField("title") != null) {
+        		System.out.println( "title" + entry.getField("title"));
+                //dialog.addEntry(entry);
+               // dialog.setProgress(parsed + unparseable, hits);
+                parsed++;
+            }
+            entryNumber++;
+            try {
+            	Thread.sleep(10000);//wait between requests or you will be blocked by ACM
+            } catch (InterruptedException e) {
+            	System.err.println(e.getStackTrace());
+            }
+        }
+    }
+	
+	private static BibtexEntry parseNextEntry(String allText, int startIndex, int entryNumber){
+		
+		String toFind = new StringBuffer().append("<strong>").append(entryNumber).append("</strong>").toString();
+		
+		int index = allText.indexOf(toFind, startIndex);
+		
+		int endIndex = allText.indexOf("</table>", index+1);
+		
+		endIndex = allText.length();
+		
+		BibtexEntry entry = null;
+		
+		System.out.println("index = " + index);
+		
+		if(index >= 0){
+			piv = index + 1;
+			String text = allText.substring(index, endIndex);
+			
+			Matcher fullCitition = fullCitationPattern.matcher(text);
+			
+			if(fullCitition.find()){
+				try {
+					Thread.sleep(1000);
+					entry  = parseEntryBibTeX(fullCitition.group(1));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}else{
+				System.out.printf("Citation Unmatched %d\n", entryNumber);
+				System.out.printf(text);
+			}
+			
+			if (entry != null) { // fetch successful
+                return entry;
+            }
+			
+		}
+		return null;
+	}
+	
+	private static BibtexEntry parseEntryBibTeX(String fullCitation){
+		URL url;
+		
+		try {
+			url = new URL(startUrl + fullCitation);
+			String page = getFetcherResult(url);
+			
+			System.out.println("url = " + url);
+			
+			Thread.sleep(10000);
+			
+			Matcher bibtexAddr = bibPattern.matcher(page);
+			
+			if(bibtexAddr.find()){
+				URL bibtexUrl = new URL(startUrl + bibtexAddr.group(1));
+				
+				BufferedReader in = new BufferedReader(new InputStreamReader(bibtexUrl.openStream()));
+				
+				ParserResult result = BibtexParser.parse(in);
+				
+				in.close();
+				Collection<BibtexEntry> item = result.getDatabase().getEntries();
+				
+				BibtexEntry entry = item.iterator().next();
+				
+				// get abstract
+				Matcher absMatch = absPattern.matcher(page);
+				if (absMatch.find()) {
+					String absBlock = absMatch.group(1);
+					entry.setField("abstract", convertHTMLChars(absBlock).trim());
+				} else {
+					System.out.println("No abstract matched.");
+					//System.out.println(page);
+				}
+				
+				Thread.sleep(10000);//wait between requests or you will be blocked by ACM
+				System.out.println("Title : " + entry.getField("title"));
+				System.out.println("Year : " + entry.getField("year"));
+				System.out.println("Abstract : " + entry.getField("abstract"));
+				
+				return entry;
+				
+			}else{
+				return null;
+			}
+			
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+     * This method must convert HTML style char sequences to normal characters.
+     * @param text The text to handle.
+     * @return The converted text.
+     */
+    private static String convertHTMLChars(String text) {
+
+        return htmlConverter.format(text);
+    }
+    
 }
