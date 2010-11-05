199fb2f098f25425c3adb72d5ea3494b39d3f370
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-11-05 05:49:04 +0000

    Model

---
+++ Source code/finalProject/src/uit/tkorg/dbsa/model/DBSAPublication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uit.tkorg.dbsa.model;
+
+/**
+ * @author tiendv
+ *
+ */
+public class DBSAPublication {
+	private int id;
+	private int sbj_id;
+	private String abstractPub;
+	private String title;
+	private int year;
+	private String publisher;
+	private String author;
+	private String links;
+
+}
---
+++ Source code/finalProject/src/uit/tkorg/dbsa/model/Subject.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package uit.tkorg.dbsa.model;
+
+/**
+ * @author tiendv
+ *
+ */
+public class Subject {
+	private int id;
+	private String sbj_name;
+}
