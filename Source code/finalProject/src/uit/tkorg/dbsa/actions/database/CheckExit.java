8953fbc51e4dab1e2d6c093c62b6c214a40b6a0a
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-11-10 02:12:03 +0000

    add package check and insert database

---
+++ Source code/finalProject/src/uit/tkorg/dbsa/actions/database/CheckExit.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package uit.tkorg.dbsa.actions.database;
+
+/**
+ * @author TKORG-PC1
+ *
+ */
+public class CheckExit {
+	/**
+	 *  
+	 * @param title
+	 * @return
+	 */
+	private String RemoveCharactor(String title){
+		String result= "";
+		if(title== "")
+			return title;
+		else {
+			
+		}
+		return result;
+		
+	}
+
+}
