66f334c8ad0563feeb484c26293036079dd6ecc4
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-11-10 08:14:49 +0000

    code

--- trunk/Source code/finalProject/src/uit/tkorg/dbsa/actions/database/InsertSubject.java
+++ trunk/Source code/finalProject/src/uit/tkorg/dbsa/actions/database/InsertSubject.java
@@ -1,7 +1,14 @@
-/**
- * 
- */
+
 package uit.tkorg.dbsa.actions.database;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import uit.tkorg.dbsa.cores.hibernate.HibernateUtil;
+import uit.tkorg.dbsa.model.Subject;;
 
 /**
  * @author tiendv
@@ -9,4 +16,27 @@ package uit.tkorg.dbsa.actions.database;
  */
 public class InsertSubject {
 
+	public static void InsertSubjectOfPublication(ArrayList<Subject> sb) {
+		Session session = null;
+		try
+		{
+		  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		  session = sessionFactory.openSession();
+		  for(int i = 0; i< sb.size();i++)
+		  {
+			  Transaction tx = session.beginTransaction();
+			  session.save(sb.get(i));
+			  tx.commit();
+		  }
+		  
+		  // Show thanh cong cho nguoi dung biet
+		 
+		}catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			// Actual Public insertion will happen at this step
+			session.flush();
+			session.close();
+			}
+	}
 }
