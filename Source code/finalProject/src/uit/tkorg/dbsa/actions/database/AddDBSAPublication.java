007ade80eeabaa72c40d0206b945e58342d62b06
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-11-10 06:45:01 +0000

    check and insert data

---
+++ Source code/finalProject/src/uit/tkorg/dbsa/actions/database/AddDBSAPublication.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package uit.tkorg.dbsa.actions.database;
+
+import java.util.ArrayList;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import uit.tkorg.dbsa.cores.hibernate.HibernateUtil;
+import uit.tkorg.dbsa.model.DBSAPublication;
+
+/**
+ * @author tiendv
+ * 
+ * 
+ *
+ */
+public class AddDBSAPublication {
+	public static void Adddatabase(ArrayList<DBSAPublication> pub) {
+		Session session = null;
+		try
+		{
+		  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		  session = sessionFactory.openSession();
+		  for(int i = 0; i< pub.size();i++)
+		  {
+			  Transaction tx = session.beginTransaction();
+			  session.save(pub.get(i));
+			  tx.commit();
+		  }
+		  
+		  // Show thanh cong cho nguoi dung biet
+		 
+		}catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			// Actual Public insertion will happen at this step
+			session.flush();
+			session.close();
+			
+			}
+	}
+	/* public static void main(String[] args) {
+		 	Session session = null;
+	        DBSAPublication test = new DBSAPublication();
+	       // test.setId(2);
+	        test.setTitle(" the Design and Implementation of Object Databases.");
+	        test.setYear(2000);
+	        try {
+	        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			session = sessionFactory.openSession();
+			Transaction tx = session.beginTransaction();
+			session.save(test);
+			 tx.commit();
+			System.out.println(test.getId());
+	        }finally {
+	        	session.flush();
+				session.close();
+	        }
+			
+	    }*/
+
+}
--- Source code/finalProject/src/uit/tkorg/dbsa/actions/database/CheckExit.java
+++ Source code/finalProject/src/uit/tkorg/dbsa/actions/database/CheckExit.java
@@ -3,24 +3,80 @@
  */
 package uit.tkorg.dbsa.actions.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import uit.tkorg.dbsa.cores.hibernate.HibernateUtil;
+import uit.tkorg.dbsa.model.DBSAPublication;
+
 /**
- * @author TKORG-PC1
+ * @author tiendv
  *
  */
 public class CheckExit {
+	static Session session = null;
+	
 	/**
-	 *  
-	 * @param title
-	 * @return
+	 * 
+	 * @param pub danh sach cac bai bao dua vao de kiem tra
+	 * 
+	 * @return ArrayList neu bai bao ton tai
+	 *  se tra ve  id tuong ung trong danh sach  kiem tra. Danh sach tra ve rong
+	 *  neu bai bao ko co trong du lieu dblp
 	 */
-	private String RemoveCharactor(String title){
-		String result= "";
-		if(title== "")
-			return title;
-		else {
-			
+	private ArrayList<Integer> CheckTitlePublications(ArrayList<DBSAPublication> pub) {
+		ArrayList<Integer> result = null;
+		for(int i =0; i<pub.size();i++){
+			if(CheckTitilePublication(pub.get(i).getTitle()) == true)
+				result.add(i);
 		}
-		return result;
+		return result;	
+	}
+	/**
+	 * 
+	 * @param tittle : title cua bai bao dua vao kiem tra
+	 * @return : true neu bai bao co trong dblp
+	 */
+	private static Boolean CheckTitilePublication(String tittle) {
+		try
+		{
+		  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		  session = sessionFactory.openSession();
+		  org.hibernate.Query q = session.createQuery("from Publication pub where pub.title = :var");
+	      q.setString("var", tittle);
+	      List result = q.list();
+	      if(result.isEmpty())
+	    	  return false;// bai bao khong co trong dblp
+	      
+		}catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			session.close();
+			}
+		return true;//// bai bao co trong dblp
+		
+	}
+	private static Boolean CheckYearPublication(DBSAPublication publ) {
+		try
+		{
+		  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		  session = sessionFactory.openSession();
+		  org.hibernate.Query q = session.createQuery("from Publication pub where pub.title = :var and pub.year =?");
+	      q.setString("var", publ.getPublisher());
+	      q.setInteger("?", publ.getYear());
+	      List result = q.list();
+	      if(result.isEmpty())
+	    	  return false;// it Contanin
+	      
+		}catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			session.close();
+			}
+		return true;
 		
 	}
 
