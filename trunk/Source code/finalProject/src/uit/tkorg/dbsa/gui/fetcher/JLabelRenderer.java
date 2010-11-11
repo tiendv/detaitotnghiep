f5f92a7a9b3e8f87c54f64f74064629ef5dbc09d
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2010-11-11 06:01:58 +0000

    comit

---
+++ trunk/Source code/finalProject/src/uit/tkorg/dbsa/gui/fetcher/JLabelRenderer.java
@@ -0,0 +1,49 @@
+package uit.tkorg.dbsa.gui.fetcher;
+import javax.swing.*;                          
+import java.awt.Component;
+import javax.swing.table.*;
+
+public class JLabelRenderer extends JLabel implements TableCellRenderer
+{
+  private MyJTable myTable;
+  /**
+   * Creates a Custom JLabel Cell Renderer
+   * @param t your JTable implmentation that holds the Hashtable to inquire for
+   * rows and colors to paint.
+   */
+  public JLabelRenderer(MyJTable t)
+  {
+    this.myTable = t;
+  }
+
+  /**
+   * Returns the component used for drawing the cell.  This method is
+   * used to configure the renderer appropriately before drawing.
+   * see TableCellRenderer.getTableCellRendererComponent(...); for more comments on the method
+   */
+  public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+  {
+    setOpaque(true); //JLabel isn't opaque by default
+    setText(value.toString());
+    setFont(myTable.getFont());
+
+    //Colors
+
+    if(!isSelected)//if the row is not selected then use the custom color
+    setBackground(myTable.getRowToPaint(row));
+    else //if the row is selected use the default selection color
+    setBackground(myTable.getSelectionBackground());
+
+    //Foreground could be changed using another Hashtable...
+    setForeground(myTable.getForeground());
+
+    // Since the renderer is a component, return itself
+    return this;
+  }
+
+  // The following methods override the defaults for performance reasons
+  public void validate() {}
+  public void revalidate() {}
+  protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {}
+  public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {}
+}
