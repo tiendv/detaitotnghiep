12731326285ef0ee8b1578c98b9c0eb3667a30b8
tiendo.vn@gmail.com <tiendo.vn@gmail.com@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2011-02-14 01:55:49 +0000

    action for get parameters for statistic tab

---
+++ trunk/Source code/finalProject/src/uit/tkorg/dbsa/actions/database/GetParametersForStatisticTab.java
@@ -0,0 +1,50 @@
+
+/**
+ * @author tiendv
+ * Get parameters for static tab
+ *
+ */
+
+package uit.tkorg.dbsa.actions.database;
+import java.util.ArrayList;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
+import uit.tkorg.dbsa.cores.hibernate.HibernateUtil;
+public class GetParametersForStatisticTab {
+
+	public static int getNumberOfPublicationsInDBLP(){ 
+		int numberofpublication = 0;
+		Session session = null;
+			try {
+				SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+				session = sessionFactory.openSession();
+				numberofpublication = ((Long)session.createQuery("select count(*) from Publication").uniqueResult()).intValue();
+			} finally {
+				session.close();
+			}
+		return numberofpublication;
+		}
+	
+	public static int getNumberOfPublicationInDBSA(){
+		int nuberofpublicationinDBSA = 0;
+		Session session = null;
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			session = sessionFactory.openSession();
+			nuberofpublicationinDBSA = ((Long)session.createQuery("select count(*) from DBSAPublication").uniqueResult()).intValue();
+		} finally {
+			session.close();
+		}
+		return nuberofpublicationinDBSA;
+	}
+	
+	public static void main(String[] args) {
+		Session session = null;
+		GetParametersForStatisticTab test = new GetParametersForStatisticTab();
+		int count =  test.getNumberOfPublicationsInDBLP();
+		int count1 =  test.getNumberOfPublicationInDBSA();
+		System.out.printf("so ket qua trong dblp la:"+ count);
+		System.out.printf("so ket qua trong DBSA la:"+ count1);
+ 	}
+}
