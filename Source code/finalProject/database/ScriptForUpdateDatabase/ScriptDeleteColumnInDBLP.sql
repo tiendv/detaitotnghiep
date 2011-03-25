dbccc06d5f7b78f71ca873339d8b221dbd54b872
tiendo.vn <tiendo.vn@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2011-03-25 05:49:53 +0000

    update script

--- Source code/finalProject/database/ScriptForUpdateDatabase/ScriptDeleteColumnInDBLP.sql
+++ Source code/finalProject/database/ScriptForUpdateDatabase/ScriptDeleteColumnInDBLP.sql
@@ -1,3 +1,3 @@
-DROP TABLE IF EXISTS 'dblp_ref_new'; 
-DROP TABLE IF EXISTS 'dblp_pub_new';
-DROP TABLE IF EXISTS 'dblp_pub_author_ref_new';
+DROP TABLE dblp_ref_new 
+DROP TABLE dblp_pub_new
+DROP TABLE dblp_pub_author_ref_new
