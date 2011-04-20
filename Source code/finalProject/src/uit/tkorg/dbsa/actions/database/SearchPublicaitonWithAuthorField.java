9297e7972b837e2f0a8e5c19549d42a5f3f94205
tiendo.vn@gmail.com <tiendo.vn@gmail.com@e3d03459-f1fa-3f55-001d-9d8307e9619b>
2011-04-20 06:31:27 +0000

    add seach publication with author name

---
+++ Source code/finalProject/src/uit/tkorg/dbsa/actions/database/SearchPublicaitonWithAuthorField.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package uit.tkorg.dbsa.actions.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import uit.tkorg.dbsa.model.Author;
+import uit.tkorg.dbsa.model.DBSAPublication;
+import uit.tkorg.dbsa.model.Publication;
+
+/**
+ * @author tiendv
+ *
+ */
+public class SearchPublicaitonWithAuthorField {
+	
+	public static ArrayList<Publication> getDBLPPublicaitonWithAuthorName (String authorName) {
+		ArrayList<Publication> result = new ArrayList<Publication>();
+		Session session = null;
+		try {
+			 Query q = session.createQuery("from Author au where au.author = :var");
+		        q.setString("var", authorName);
+		        List authors = q.list();
+			if(authors.isEmpty())
+				return null;
+			else {
+					List<Publication> pubs = new ArrayList<Publication>();
+						for (int i = 0; i < authors.size(); i++) {
+							Author au = (Author) authors.get(i);
+							Publication mypub = au.getPublications();
+							result.add(mypub);
+			        }
+					return result;
+			}
+			
+		} finally {
+			session.close();
+		}	
+	}
+	public static ArrayList<DBSAPublication> getDBSAPublicaitonWithAuthorName (String authorName) {
+		ArrayList<DBSAPublication> result = new ArrayList<DBSAPublication>();
+		Session session = null;
+		try {
+			 Query q = session.createQuery("from Author au where au.author = :var");
+		        q.setString("var", authorName);
+		        List authors = q.list();
+			if(authors.isEmpty())
+				return null;
+			else {
+				return result;
+			}
+			
+		} finally {
+			session.close();
+		}	
+	}
+
+}
